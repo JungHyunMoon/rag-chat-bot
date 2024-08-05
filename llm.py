c0039037d4ff6cfe3b404ad435e58e7015f0e005
Junghyun <foqlzm12345@direa.co.kr>
2024-08-05 12:25:35 +0900

    feat : LLM Customize
    1. 문서 출처작성 프롬포트 추가
    2. 문서 사전 약식 구현 (추가 작업 필요)

--- llm.py
+++ llm.py
@@ -1,5 +1,6 @@
 from langchain_core.output_parsers import StrOutputParser
-from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder, FewShotChatMessagePromptTemplate
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder, FewShotChatMessagePromptTemplate, \
+    PromptTemplate
 from langchain.chains import create_history_aware_retriever, create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_openai import ChatOpenAI
@@ -24,15 +25,17 @@ def get_session_history(session_id: str) -> BaseChatMessageHistory:
 def get_retriever():
     embedding = OpenAIEmbeddings(model='text-embedding-3-large')
     index_name = 'wiki-openai-index'
-    namespace = "doc_v1"
+    namespace = "doc_v2"
     database = PineconeVectorStore.from_existing_index(index_name=index_name, namespace=namespace, embedding=embedding)
-    retriever = database.as_retriever(search_kwargs={'k': 4})
+    retriever = database.as_retriever(search_kwargs={'k': 4}, return_source_documents=True)
     return retriever
+
 
 def get_history_retriever():
     llm = get_llm()
     retriever = get_retriever()
 
+    # 체팅 내역 유지를 위한 검증된 프롬프트 사용
     contextualize_q_system_prompt = (
         "Given a chat history and the latest user question "
         "which might reference context in the chat history, "
@@ -61,7 +64,7 @@ def get_llm(model='gpt-4o-mini'):
 
 
 def get_dictionary_chain():
-    dictionary = ["사람을 나타내는 표현 -> 거주자"]
+    dictionary = ["Link를 나타내는 표현 -> CruzLink",]
     llm = get_llm()
     prompt = ChatPromptTemplate.from_template(f"""
         사용자의 질문을 보고, 우리의 사전을 참고해서 사용자의 질문을 변경해주세요.
@@ -89,15 +92,14 @@ def get_rag_chain():
         example_prompt=example_prompt,
         examples=answer_examples,
     )
-    system_prompt = (
-        "당신은 똑똑한 디리아 상담 챗봇입니다. 사용자의 디리아 솔루션에 관한 질문에 답변해주세요"
-        "아래에 제공된 문서를 활용해서 답변해주시고"
-        "답변을 알 수 없다면 모른다고 답변해주세요"
-        # "답변을 제공할 때는 소득세법 (XX조)에 따르면 이라고 시작하면서 답변해주시고"
-        # "2-3 문장정도의 짧은 내용의 답변을 원합니다"
-        "\n\n"
-        "{context}"
-    )
+    system_prompt = ("""
+        당신은 똑똑한 디리아 상담 챗봇입니다. 사용자의 디리아(Direa)에 관한 질문에 답변해주세요
+        답변은 아래의 규칙을 따라서 답변해주세요
+        1. 모든 답변에는 직접적으로 참조한 자료의 목록을 추가해야 합니다. 최종 답변에 관련이 없는 자료는 제외합니다.
+        2. 답을 모를 경우 답을 지어내지 말고 [정확한 답을 찾을 수 없지만, 다음 링크를 확인해 보시기 바랍니다] 라고 말한 뒤 자료 링크를 목록으로 추가합니다.        "\n\n"
+        {context}
+        """
+                     )
 
     qa_prompt = ChatPromptTemplate.from_messages(
         [
@@ -108,7 +110,18 @@ def get_rag_chain():
         ]
     )
     history_aware_retriever = get_history_retriever()
-    question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
+
+    # 답변에 소스 포함하기 위한 프롬프트
+    document_prompt = PromptTemplate(
+        input_variables=["page_content", "source"],
+        template="Context:\ncontent:{page_content}\nsource:{source}",
+    )
+
+    question_answer_chain = create_stuff_documents_chain(
+        llm=llm,
+        prompt=qa_prompt,
+        document_prompt=document_prompt,
+    )
 
     rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
 
